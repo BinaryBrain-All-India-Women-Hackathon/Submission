94afe8a3e4320bdc46fa807f516ee5b3057ff7ac
KEERTHANA S <95169415+KeerthanaS04@users.noreply.github.com>
2023-08-06 10:34:29 +0530

    Delete web_development/Hackathon- all india women directory

--- web_development/Hackathon- all india women/About.js
+++
@@ -1,8 +0,0 @@
-// Toggle FAQ answers when clicked
-const faqItems = document.querySelectorAll('.faq-section .faq');
-
-faqItems.forEach(item => {
-    item.addEventListener('click', () => {
-        item.classList.toggle('active');
-    });
-});
